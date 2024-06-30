85fd2970a4e8d044b1d3951a1a472ec711f1bc0f
unknown <shashwotpradhan@gmail.com>
2024-06-30 08:02:43 +0545

    clerk setup and nav items

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import { ClerkProvider, SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton } from "@clerk/nextjs";
 import "./globals.css";
 
 const poppins = Poppins({ 
@@ -22,9 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    
-    <html lang="en">
-      <body className={poppins.variable}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+      
+        <body className={poppins.variable}>
+        {/* <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn> */}
+          {children}
+          </body>
+      </html>
+
+    </ClerkProvider>
   );
 }
