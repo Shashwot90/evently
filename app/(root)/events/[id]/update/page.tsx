5bcedc51d52a905fe2ec40153e9a8d2cdd75a668
unknown <shashwotpradhan@gmail.com>
2024-07-02 14:17:24 +0545

    create and update event initialize

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -2,20 +2,20 @@ import EventForm from '@/components/shared/EventForm'
 import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 
-const CreateEvent = () => {
+const UpdateEvent = () => {
     const { sessionClaims } = auth()
     const userId = sessionClaims?.userId as string 
     
   return (
     <>
     <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10'>
-        <h3 className="wrapper h3-bold  text-center sm:text-left">Crete Event</h3>
+        <h3 className="wrapper h3-bold  text-center sm:text-left">Update Event</h3>
     </section>
     <div className="wrapper my-8">
-        <EventForm userId={userId} type='Create' />
+        <EventForm userId={userId} type='Update' />
     </div>
     </>
   )
 }
 
-export default CreateEvent
+export default UpdateEvent
