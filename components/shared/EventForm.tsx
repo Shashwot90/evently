4a188be2e71a83e79a6ab2474882e7858ce4df24
unknown <shashwotpradhan@gmail.com>
2024-07-05 13:21:02 +0545

    upload pic

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -12,6 +12,7 @@ import Dropdown from "./Dropdown"
 import { Textarea } from "@/components/ui/textarea"
 import {FileUploader} from "./FileUploader"
 import { useState } from "react"
+import Image from "next/image"
 
 
 
@@ -103,6 +104,57 @@ const EventForm = ({ userId, type}: EventFormProps) => {
               )}
               />
         </div>
+        <div className="flex flex-col gap-5 md:flex-row">
+            <FormField
+                control={form.control}
+                name="location"
+                render={({ field }) => (
+                    <FormItem className="w-full">
+                    
+                    <FormControl>
+                        <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                            <Image 
+                                src="/assets/icons/location-grey.svg"
+                                alt="calendar"
+                                width={24}
+                                height={24} />
+                            <Input placeholder="Event Location or Online" {...field} className="input-field" />
+                        </div>
+                    </FormControl>
+                    
+                    <FormMessage />
+                    </FormItem>
+                )}
+                />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+            <FormField
+                control={form.control}
+                name="startDateTime"
+                render={({ field }) => (
+                    <FormItem className="w-full">
+                    
+                    <FormControl>
+                        <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                            <Image 
+                                src="/assets/icons/calendar.svg"
+                                alt="calendar"
+                                width={24}
+                                height={24}
+                                className="filter-grey" />
+                                <p className="ml-3 whitespace-nowrap text-grey-600">Start Date:</p>
+                                
+                            
+                        </div>
+                    </FormControl>
+                    
+                    <FormMessage />
+                    </FormItem>
+                )}
+                />
+        </div>
+
         <Button type="submit">Submit</Button>
       </form>
     </Form>
